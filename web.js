4fd5d31adb40766696943c74cd1c5cb0abe311be
Eric Bencomo <ericbencomod@gmail.com>
2013-07-09 23:04:57 +0000

    Modified web.js

--- web.js
+++ web.js
@@ -10,10 +10,10 @@ app.get('/', function(request, response) {
 
 
 app.get('/', function(request, response) {
-  fs.readFileSync('./index.html', function (err, data) {
+//  fs.readFileSync('./index.html', function (err, data) {
 //    if (err) throw err;
 //    response.send(data.toString('utf-8'));
-  });
+  //});
 });
 
 
